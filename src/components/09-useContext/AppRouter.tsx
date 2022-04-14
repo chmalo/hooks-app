c0e075098a695cdb066e3883ad08c8c2297c7770
Chmalo <chmalo.f@gmail.com>
2022-04-14 08:07:29 -0400

    feat: useContext v1

---
+++ src/components/09-useContext/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { AboutScreen } from "./AboutScreen"
+import { LoginScreen } from "./LoginScreen"
+import { HomeScreen } from "./HomeScreen"
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+
+          <Route exact path="/about" component={AboutScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
